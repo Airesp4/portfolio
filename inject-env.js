3f9b416cbfc680c93b9785a3b09f25533c56382a
Pedro Augusto <pedroaugustogoncalvesribeiro@gmail.com>
2025-07-28 15:55:53 -0300

    Adequando projeto para Deploy

---
+++ inject-env.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const token = process.env.GITHUB_TOKEN;
+
+if (!token) {
+  throw new Error('❌ GITHUB_TOKEN não definido');
+}
+
+const content = `
+export const environment = {
+  production: true,
+  githubToken: '${token}'
+};
+`;
+
+fs.writeFileSync('./src/environments/environment.ts', content);
+console.log('✅ Token injetado com sucesso!');
